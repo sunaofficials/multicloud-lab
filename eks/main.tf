d0109ce4f9ff7f2bcce842040bc0642e1ce37761
Santa <ssn@cisco.com>
2026-02-17 20:41:02 +0530

    add az data soufdrcees

--- eks/main.tf
+++ eks/main.tf
@@ -8,48 +8,8 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  # Public endpoint enabled (can restrict CIDR later)
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
   enable_irsa = true
-}
-
-# -------------------------
-# EKS Data Sources
-# -------------------------
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
-
-# -------------------------
-# Helm Provider
-# -------------------------
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(
-      data.aws_eks_cluster.cluster.certificate_authority[0].data
-    )
-    token = data.aws_eks_cluster_auth.cluster.token
-  }
-}
-
-# -------------------------
-# Kubectl Provider
-# -------------------------
-
-provider "kubectl" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(
-    data.aws_eks_cluster.cluster.certificate_authority[0].data
-  )
-  token            = data.aws_eks_cluster_auth.cluster.token
-  load_config_file = false
 }
--- eks/provider_variables.tf
+++ eks/provider_variables.tf
@@ -6,16 +6,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.12"
-    }
-
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~> 1.14"
-    }
   }
 }
 
@@ -24,24 +14,20 @@ terraform {
 # -------------------------
 
 variable "cluster_name" {
-  description = "EKS Cluster Name"
-  type        = string
+  type = string
 }
 
 variable "region" {
-  description = "AWS Region"
-  type        = string
+  type = string
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR Block"
-  type        = string
+  type = string
 }
 
 variable "cluster_version" {
-  description = "Kubernetes Version"
-  type        = string
-  default     = "1.32"
+  type    = string
+  default = "1.32"
 }
 
 # -------------------------
@@ -52,14 +38,7 @@ provider "aws" {
   region = var.region
 }
 
-# Needed for subnet creation
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
-
-variable "node_instance_type" {
-  description = "Instance type for Karpenter nodes"
-  type        = string
-  default     = "t3.medium"
-}
