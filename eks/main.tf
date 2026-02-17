4a343b976354ae2a8eb23aea7a9d45462170a342
Santa <ssn@cisco.com>
2026-02-17 19:47:19 +0530

    frwessssss

---
+++ eks/main.tf
@@ -0,0 +1,32 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  # Public endpoint enabled (restricted later)
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  enable_irsa = true
+}
+
+# Kubernetes provider (after cluster creation)
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = module.eks.cluster_auth_token
+  }
+}
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = module.eks.cluster_auth_token
+  load_config_file       = false
+}
