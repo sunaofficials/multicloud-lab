187d9c6daf691d4d6e75b64fe927250f0fae16b9
Santa <ssn@cisco.com>
2026-02-17 20:19:11 +0530

    frwessssfdsfsss

--- eks/provider_variables.tf
+++ eks/provider_variables.tf
@@ -1,18 +1,26 @@
-terraform {
-  required_version = ">= 1.5.0"
+variable "cluster_name" {
+  description = "EKS Cluster Name"
+  type        = string
+}
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.12"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~> 1.14"
-    }
-  }
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes Version"
+  type        = string
+  default     = "1.32"
+}
+
+variable "node_instance_type" {
+  description = "Node Instance Type"
+  type        = string
+  default     = "t3.medium"
 }
